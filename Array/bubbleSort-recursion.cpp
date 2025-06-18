8b7107146f9ce2f953c10fd9a85f8779ee78574f
Ayush Malviya <ayushmalviya078@gmail.com>
2025-06-19 00:00:14 +0530

    Add files via upload

---
+++ Array/bubbleSort-recursion.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void bubble_sort(int arr[], int n) {
+    // Base Case: range == 1.
+    if (n == 1) return;
+
+    for (int j = 0; j <= n - 2; j++) {
+        if (arr[j] > arr[j + 1]) {
+            int temp = arr[j + 1];
+            arr[j + 1] = arr[j];
+            arr[j] = temp;
+        }
+    }
+
+    //Range reduced after recursion:
+    bubble_sort(arr, n - 1);
+}
+
+int main()
+{
+      int n;
+    cin >> n;
+    int arr[n];
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    bubble_sort(arr, n);
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << "\n";
+    return 0;
+
+}
